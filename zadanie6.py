9364cb3c88538fbf1a12949cdbc0d2458cc1d2a7
szczupaczek132 <maxpuchatek@icloud.com>
2024-11-21 11:37:39 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,4 @@
+#Pętla while wykonuje się tak długo, jak długo spełniony jest dany warunek. Zaczyna się od sprawdzenia warunku, jeśli jest True, to wykonuje się ciało pętli, a potem znów sprawdza warunek.
+#Pętla for jest używana, gdy chcesz wykonać jakąś operację dla każdego elementu w określonej sekwencji, np. w liście, ciągu znaków, zakresie liczb itp.
+#for: używamy, gdy wiemy, ile razy coś ma się powtórzyć lub mamy sekwencję, po której chcemy przejść.
+#while: używamy, gdy chcemy powtarzać coś do momentu, aż spełniony zostanie jakiś warunek.
